38f5b4956eae9dd3e069418cd451085f9e4ed841
Aleksandr Nagornyy <freerunningpanda@yandex.ru>
2022-02-10 14:48:22 +0300

    optimized model

--- lib/models/pokemon_info.dart
+++ lib/models/pokemon_info.dart
@@ -11,30 +11,16 @@ class PokemonInfo {
   PokemonInfo({
     required this.abilities,
     required this.baseExperience,
-    required this.forms,
     required this.height,
-    required this.heldItems,
-    required this.id,
-    required this.locationAreaEncounters,
     required this.name,
-    required this.order,
-    required this.pastTypes,
-    required this.species,
     required this.sprites,
     required this.weight,
   });
 
   final List<Ability> abilities;
   final int baseExperience;
-  final List<Species> forms;
   final int height;
-  final List<dynamic> heldItems;
-  final int id;
-  final String locationAreaEncounters;
   final String name;
-  final int order;
-  final List<dynamic> pastTypes;
-  final Species species;
   final Sprites sprites;
   final int weight;
 
--- lib/models/pokemon_info.g.dart
+++ lib/models/pokemon_info.g.dart
@@ -11,17 +11,8 @@ PokemonInfo _$PokemonInfoFromJson(Map<String, dynamic> json) => PokemonInfo(
           .map((e) => Ability.fromJson(e as Map<String, dynamic>))
           .toList(),
       baseExperience: json['base_experience'] as int,
-      forms: (json['forms'] as List<dynamic>)
-          .map((e) => Species.fromJson(e as Map<String, dynamic>))
-          .toList(),
       height: json['height'] as int,
-      heldItems: json['held_items'] as List<dynamic>,
-      id: json['id'] as int,
-      locationAreaEncounters: json['location_area_encounters'] as String,
       name: json['name'] as String,
-      order: json['order'] as int,
-      pastTypes: json['past_types'] as List<dynamic>,
-      species: Species.fromJson(json['species'] as Map<String, dynamic>),
       sprites: Sprites.fromJson(json['sprites'] as Map<String, dynamic>),
       weight: json['weight'] as int,
     );
@@ -30,15 +21,8 @@ Map<String, dynamic> _$PokemonInfoToJson(PokemonInfo instance) =>
     <String, dynamic>{
       'abilities': instance.abilities.map((e) => e.toJson()).toList(),
       'base_experience': instance.baseExperience,
-      'forms': instance.forms.map((e) => e.toJson()).toList(),
       'height': instance.height,
-      'held_items': instance.heldItems,
-      'id': instance.id,
-      'location_area_encounters': instance.locationAreaEncounters,
       'name': instance.name,
-      'order': instance.order,
-      'past_types': instance.pastTypes,
-      'species': instance.species.toJson(),
       'sprites': instance.sprites.toJson(),
       'weight': instance.weight,
     };
